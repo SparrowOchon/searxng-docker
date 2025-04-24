c7ead90afa410389af2546d2b926d4d8958ab584
Network Silence <42444069+SparrowOchon@users.noreply.github.com>
2025-04-24 18:07:59 -0400

    Added a setup script and fixed some port issues

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Generate the self-signed certificate (valid for 365 days)
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+  -keyout nginx/ssl/key.pem \
+  -out nginx/ssl/cert.pem \
+  -subj '/CN=localhost/O=SearXNG Self Signed/C=US'
+
+# Set appropriate permissions
+chmod 600 nginx/ssl/key.pem
+chmod 644 nginx/ssl/cert.pem
